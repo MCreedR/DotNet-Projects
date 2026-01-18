1ef131fdbd2e2e86fd9a887ae6f59e335dcbc4d9
Creed <laurvitalik89@gmail.com>
2026-01-18 17:46:29 +0200

    feat: implement ToDo API with postgreSQL and secure configuration

---
+++ TaskManagementAPI/TaskManagementAPI/Data/ApiDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManagementAPI.Models;
+
+namespace TaskManagementAPI.Data
+{
+    public class ApiDbContext : DbContext
+    {
+        public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Models.ToDoItem> ToDoItems { get; set; } 
+    }
+}
