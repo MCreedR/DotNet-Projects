95d650f90da406c39effa436b77cf361d9b21f19
Creed <laurvitalik89@gmail.com>
2026-01-29 19:37:04 +0200

    feat[dotnet]: Implement TemplateController and configure Swagger

---
+++ NotificationMicroService/NotificationMicroService/Controllers/TemplatesController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NotificationMicroService.Data;
+using NotificationMicroService.Models;
+
+namespace NotificationMicroService.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TemplatesController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public TemplatesController(AppDbContext ctx)
+        {
+            _context = ctx;
+        }
+
+        //GET: api/templates
+        //Purpose: get all notification templates for the UI
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<NotificationTemplate>>> GetTemplates()
+        {
+            return await _context.NotificationTemplates.ToListAsync();
+        }
+
+        //Post: api/templates
+        //Purpose: create a new notification template
+        [HttpPost]
+        public async Task<ActionResult<NotificationTemplate>> CreateTemplate(NotificationTemplate template)
+        {
+            _context.NotificationTemplates.Add(template);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetTemplates), new { id = template.Id }, template);
+
+        }
+    }
+}
--- NotificationMicroService/NotificationMicroService/Program.cs
+++ NotificationMicroService/NotificationMicroService/Program.cs
@@ -8,8 +8,14 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
 
 var app = builder.Build();
 
@@ -17,8 +23,15 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
-}
 
+    app.UseSwagger();
+
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "Swagger");
+    });
+}
+ 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
