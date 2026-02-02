33499c4a3ae44c4e11f39df0302c795aa3aec26d
Creed <laurvitalik89@gmail.com>
2026-02-02 20:06:30 +0200

    fix[dotnet]: Update CORS policy for Vite port 5173 and add method GetLogs in NotificationsController

--- NotificationMicroService/NotificationMicroService/Controllers/NotificationsController.cs
+++ NotificationMicroService/NotificationMicroService/Controllers/NotificationsController.cs
@@ -1,10 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NotificationMicroService.Data;
 using NotificationMicroService.Services;
 
 namespace NotificationMicroService.Controllers
 {
+    [ApiController]
+    [Route("api/[controller]")]
     public class NotificationsController : Controller
     {
+        private readonly AppDbContext _context;
+
+        public NotificationsController(AppDbContext ctx)
+        {
+            _context = ctx;
+        }
+
         [HttpPost("send")]
         public async Task<IActionResult> Send([FromServices] NotificationService service, int templateId, string recipient)
         {
@@ -13,6 +24,16 @@ namespace NotificationMicroService.Controllers
                 return Ok("Notification sent successfully.");
             else
                 return BadRequest("Failed to send notification.");
+        }
+
+        [HttpGet("logs")]
+        public async Task<IActionResult> GetLogs()
+        {
+            var logs = await _context.NotificationLogs
+                .OrderByDescending(l => l.SentAt)
+                .ToListAsync();
+
+            return Ok(logs);
         }
     }
 }
